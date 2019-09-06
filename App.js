e7e87c35e6d0cf302f8056336c4373baac5f2061
Luong Quan <luong.quan@mulodo.com>
2019-09-06 13:23:58 +0700

    - Import redux - create Action, Reducer, Store, dispatch, subscribe - Các cách tạo action

--- App.js
+++ App.js
@@ -1,114 +1,117 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+/* eslint-disable prettier/prettier */
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import {createStore} from 'redux';
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+// State
+// Tự tạo thêm 1 cái mảng chứa history của state
+// Mảng ban đầu có 1 phần tử với giá trị là 1
+let appState = {stateValue: 1, stateHistory: [1]};
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
+// Action
+const _functionAdd = {
+  type: 'TYPE_ADD',
+  actionValue: 1,
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+const _functionSub = {
+  type: 'TYPE_SUB',
+  actionValue: 1,
+};
+
+// Reducer
+const numberReducer = (state, action) => {
+  switch (action.type) {
+    case 'TYPE_ADD':
+      // CÁCH mutable state
+      // state.stateValue += action.actionValue;
+      //---------------------------
+      // CÁCH immutable state
+      // Để lấy lại được các state cũ lun
+      // ...state có nghĩa là giữ lại các state cũ
+      const newValue = state.stateValue + action.actionValue
+      state = {
+        ...state,
+        // Ở đây mình sẽ update cái stateHistory
+        // với: ...state.stateHistory là giữ lại các phần tử đã có trong mảng
+        // và thêm 1 phần tử mới newValue vào
+        stateHistory: [...state.stateHistory, newValue],
+        // Cách cũ tương đương với câu lệnh: 
+        // state.stateValue = state.stateValue + action.actionValue
+        // Cách cũ:
+        //stateValue: state.stateValue + action.actionValue
+        // Cách mới: 
+        stateValue: newValue,
+      }
+      break;
+    case 'TYPE_SUB':
+      // CÁCH mutable state
+      // state.stateValue -= action.actionValue;
+      //---------------------------
+      // CÁCH immutable state
+      // Để lấy lại được các state cũ lun
+      // ...state có nghĩa là giữ lại các state cũ
+      const newVal = state.stateValue - action.actionValue
+      state = {
+        ...state,
+        // Ở đây mình sẽ update cái stateHistory
+        // với: ...state.stateHistory là giữ lại các phần tử đã có trong mảng
+        // và thêm 1 phần tử mới newValue vào
+        stateHistory: [...state.stateHistory, newVal],
+        // Cách cũ tương đương với câu lệnh:
+        // state.stateValue = state.stateValue + action.actionValue
+        // Cách cũ:
+        //stateValue: state.stateValue + action.actionValue
+        // Cách mới:
+        stateValue: newVal,
+      }
+      break;
+  }
+  return state;
+};
+
+// Store
+const store = createStore(numberReducer, appState);
+
+// Test
+store.subscribe(() => {
+  // Để đăng ký lắng nghe state thay đổi
+  // Mỗi khi state thay đổi sẽ vô đây
+  console.log('Store updated', store.getState());
 });
 
-export default App;
+// Cách Action dạng biến
+// MỖI LẦN GỌI DISPATCH callback subcribe sẽ được gọi.
+store.dispatch(_functionAdd); // Output: 2 (ban đầu là 1 + 1 => 2)
+store.dispatch(_functionAdd); // Output: 3
+store.dispatch(_functionAdd); // Output: 4
+store.dispatch(_functionSub); // Output: 3
+
+// Cách Action dạng anonymous
+// Mình sử dụng lại action type cũ 'TYPE_ADD' và thay đổi giá trị truyền vào.
+// Mình tạo ra 1 anonymous object
+store.dispatch({
+  type: 'TYPE_ADD',
+  actionValue: 8,
+}); // Output: 11
+
+// Cách Action dạng 1 anonymous function return kiểu trả về là 1 action
+const hamCreateActionAdd = (inputNumber) => {
+  return {type: 'TYPE_ADD', actionValue: inputNumber};
+};
+store.dispatch(hamCreateActionAdd(5)); // Output: 16
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent:'center'}}>
+        <Text>Demo</Text>
+      </View>
+    );
+  }
+}
